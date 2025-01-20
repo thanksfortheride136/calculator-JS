78c7c432722a18c2b6b55f798407e781813f32ae
Cooper <104042225+thanksfortheride136@users.noreply.github.com>
2025-01-20 18:23:47 +0000

    started js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let buttons = document.getElementsByClassName('math-buttons');
+
+function buttonValue(){
+    for(i = 0; i < buttons.length; i++){
+        const textContent = buttons[i].textContent;
+        if (textContent === "x"){
+            console.log(textContent, "found");
+        }
+    }
+}
 
 
-math-buttons.addEventListener("click", () =>{
-    console.log(math-buttons.data-value);
-});
+//iterated through the loops to get the text content, now i need to say when clicked get the text content, and do the math operation. Maybe need to convert to int.
